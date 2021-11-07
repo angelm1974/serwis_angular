204de9495ad2e56021532b346ac2018d9996e4a8
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-11-07 13:53:11 +0100

    json

---
+++ src/app/mock-physicsists.ts
@@ -0,0 +1,32 @@
+import { Physicist } from './physicist';
+
+export const PHYSICISTS: Physicist[] = [
+  {
+    nazwisko_imie: 'Maria Curie Skłodowska',
+    uniwersytet: 'Sorbona',
+    zdjecie: '././assets/images/curie.jpg',
+    opis: 'Maria Salomea Skłodowska-Curie – polsko-francuska fizyk i chemik, dwukrotna laureatka Nagrody Nobla. W 1891 Maria Skłodowska wyjechała z Królestwa Polskiego do Paryża, by podjąć studia na Sorbonie; następnie rozwinęła tam swoją karierę naukową. Była prekursorką nowej gałęzi chemii – radiochemii.',
+    polubienia: 0
+  },
+  {
+    nazwisko_imie: 'Niels Bohr',
+    uniwersytet: 'Uniwersyt Kopenhaski',
+    zdjecie: '././assets/images/bor.jpg',
+    opis: 'Niels Henrik David Bohr, IPA: [ˈnels ˈboɐ̯ˀ] – duński fizyk, laureat Nagrody Nobla w dziedzinie fizyki w 1922 za badania struktury atomu i promieniowania emitowanego przez nie. Jego prace naukowe przyczyniły się do zrozumienia budowy atomu oraz rozwoju mechaniki kwantowej',
+    polubienia: 0
+  },
+  {
+    nazwisko_imie: 'Albert Einstein',
+    uniwersytet: 'Uniwersytet w Zurychu',
+    zdjecie: '././assets/images/einstein.jpg',
+    opis: 'Einstein to twórca szczególnej teorii względności, która ulepszyła mechanikę Newtona i zastąpiła w tej korekcyjnej roli teorię eteru Lorentza[a]. Autor wynikającej z STW równoważności masy i energii, czasem formułowanej słynnym wzorem E = mc2. Został on potwierdzony przez reakcje subatomowe, np. jądrowe; doprowadziło to do rewizji zasady zachowania masy, samego pojęcia materii i otworzyło epokę jądrowej broni oraz energetyki. Twórca ogólnej teorii względności, która połączyła niutonowskie prawo powszechnego ciążenia z nową mechaniką. Nie była to jedyna ani nawet pierwsza synteza tego typu[b],',
+    polubienia: 0
+  },
+  {
+    nazwisko_imie: 'Michio Kaku',
+    uniwersytet: 'Harvard',
+    zdjecie: '././assets/images/kaku.jpg',
+    opis: ' Amerykanin pochodzenia japońskiego, fizyk teoretyk, profesor, badacz teorii strun, popularyzator nauki, futurolog, autor ponad 70 prac i wielu książek, z których kilka stało się bestsellerami, gospodarz programów radiowych i telewizyjnych. Pracuje na Uniwersytecie Nowojorskim, gdzie piastuje funkcję kierownika katedry fizyki teoretycznej.',
+    polubienia: 0
+  },
+]
---
+++ src/app/physicist.ts
@@ -0,0 +1,7 @@
+export interface Physicist {
+  nazwisko_imie: string;
+  uniwersytet: string;
+  zdjecie: string;
+  opis: string;
+  polubienia: number;
+}
